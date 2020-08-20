a5cee20de24cc024b1243b2a2af10a6f5a8378f3
Rubem <rfsv@ic.ufal.br>
2020-08-20 16:33:18 -0300

    Header added

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import MobileHeader from '../MobileHeader';
+import DesktopHeader from '../DesktopHeader';
+import LeftColumn from '../LeftColumn';
+import MiddleColumn from '../MiddleColumn';
+import RightColumn from '../RightColumn';
+import AdBanner from '../AdBanner';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <MobileHeader />
+      <DesktopHeader />
+      
+      <span>
+        <AdBanner />
+      </span>
+      
+      <main>
+        <LeftColumn />
+        <MiddleColumn />
+        <RightColumn />
+      </main>
+    </Container>
+  );
+}
+
+export default Layout;
+
+
